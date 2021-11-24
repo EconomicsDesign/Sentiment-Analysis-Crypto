54c3155c90f5222a30e22dcac95077cd7546df2e
James Pierog <james.pierog@gmail.com>
2021-11-24 19:26:00 +0800

    added environment variables to the setup

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
+from textblob import TextBlob
+import tweepy
+import sys
+import os
 
-
+T_API_KEY = os.environ.get("T_API_KEY")
+T_API_SEC = os.environ.get("T_API_SEC")
+BEARER = os.environ.get("BEARER")
 
 
 
 
 
 def main():
-    print("Hello World!")
+    print("Hello World! \n")
+    print("T_API_KEY = " + T_API_KEY)
+    print("T_API_SEC = " + T_API_SEC)
+    print("BEARER = " + BEARER)
 
 if __name__ == "__main__":
     main()
